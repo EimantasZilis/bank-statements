8387110be1f0f2998dde510ee405eff8f58dadf8
Eimantas Zilis <zilis.eimantas@gmail.com>
2019-06-06 08:29:00 +0100

    Fix: mappings were written to wrong file.

--- data/unclassified.py
+++ data/unclassified.py
@@ -1,6 +1,6 @@
 import system.file_management as fm
 
-categories = fm.Jdict("u_categories.json", system_file=True)
+categories = fm.Jdict("u_cmappings.json", system_file=True)
 classified = fm.Statements("classified")
 unclassified = fm.Statements("unclassified")
 
