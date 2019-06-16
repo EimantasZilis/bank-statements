1cb474754b0a01fa6b1311c909208017b67eefb5
Eimantas Zilis <zilis.eimantas@gmail.com>
2019-06-16 08:37:07 +0100

    Simplified get_transactions_summary

--- data/summary.py
+++ data/summary.py
@@ -21,41 +21,12 @@ def get_transactions_summary(file="Classified", filter="Total"):
         err = "{} invalid filter.\n >> Must be one of {}"
         raise ValueError(err.format(file, ", ".join(ok_files)))
 
-    summary = get_transactions_breakdown(file)
-    total_count = summary.lookup("All", filter, default=0)
-    classified_count = summary.lookup("Classified", filter, default=0)
-    unclassified_count = summary.lookup("Unclassified", filter, default=0)
-    return total_count, classified_count, unclassified_count
-
-def get_transactions_breakdown(file="classified"):
-    """ Generate a summary showing a number of classified,
-    unclassified and total transactions. Also shows the same
-    summary for unique transactions, where transactions are
-    grouped by info column. """
-
-    # Get info for all data
     all_data = Statements(file)
-    total, classified, unclassified = get_count(all_data)
-
-    # Get info for unique data
-    all_data.drop_duplicates(subset="Info")
-    total_unique, classified_unique, unclassified_unique = get_count(all_data)
-
-    summary = {
-        "All": {
-            "Total": total,
-            "Unique": total_unique
-        },
-        "Classified": {
-            "Total": classified,
-            "Unique": classified_unique
-        },
-        "Unclassified": {
-            "total": unclassified,
-            "Unique": unclassified_unique
-        }
-    }
-    return Jdict(dict=summary)
+    if filter == "Unique":
+        all_data.drop_duplicates(subset="Info")
+        
+    total_count, classified_count, unclassified_count = get_count(all_data)
+    return total_count, classified_count, unclassified_count
 
 def get_count(data):
     """ Return the number of transactions: all,
