9d9fd96fe40a26a7a5ea803c45bc127de2d8f4b3
Eimantas Zilis <zilis.eimantas@gmail.com>
2019-08-28 14:39:44 +0100

    test_file_management.py - Updated Path class tests

--- unit_tests/test_system/test_file_management.py
+++ unit_tests/test_system/test_file_management.py
@@ -1,25 +1,47 @@
 import os
-from pathlib import Path as userpath
+import pathlib
 import pytest
+import shutil
+import tempfile
+
 from system.file_management import Path
+
+
+USERPATH = str(pathlib.Path.home())
+
+@pytest.fixture
+def temp_new_dir():
+    dirpath = tempfile.mkdtemp()
+    yield dirpath
+    shutil.rmtree(dirpath)
 
 class TestPath:
     creatable = [False, False, False, False, False, True, True]
-    paths = ["what:\\x", None, 1, 0, "", str(userpath.home()),
-            os.path.splitdrive((userpath.home()))[0]]
+    paths = ["em:\\x", None, 1, 0, "", USERPATH, os.path.splitdrive(USERPATH)[0]]
 
-    @pytest.mark.parametrize("temp_path", paths)
-    def test_var_path_1(self, temp_path):
-        p_object = Path(temp_path)
-        assert p_object.path == temp_path
+    @pytest.fixture
+    def object(self, path):
+        """ Create Path object with a given path"""
+        return Path(path)
 
-    def path_exists_or_is_creatable(self, temp_path, temp_creatable):
-        p_object = Path(temp_path)
-        assert p_object.exists_or_is_creatable() == temp_creatable
+    @pytest.mark.parametrize("path", paths)
+    def test_var_path(self, object, path):
+        assert object.path == path
 
-    @pytest.mark.parametrize("tpath,tcreatable", zip(paths, creatable))
-    def test_path_exists_or_is_creatable_1(self, tpath, tcreatable):
-        self.path_exists_or_is_creatable(tpath, tcreatable)
+    @pytest.mark.parametrize("path,tcreatable", list(zip(paths, creatable)))
+    def test_fx_exists_or_is_creatable(self, object, tcreatable):
+        assert object.exists_or_is_creatable() == tcreatable
 
-    def test_path_exists_or_is_creatable_2(self, tmpdir):
-        self.path_exists_or_is_creatable(str(tmpdir), True)
+    def path_fx_init_dirs(self, dir, prev_expected, now_expected):
+        prev_exists = os.path.exists(dir)
+        path_object = Path(dir)
+        path_object.init_dirs()
+        now_exists = os.path.exists(dir)
+        assert prev_exists == prev_expected and now_exists == now_expected
+
+    def test_fx_init_dirs_1_existing(self, temp_new_dir):
+        self.path_fx_init_dirs(temp_new_dir, True, True)
+
+    def test_fx_init_dirs_2_new(self, temp_new_dir):
+        newpath = os.path.join(temp_new_dir, "subfolder")
+        self.path_fx_init_dirs(newpath, False, True)
