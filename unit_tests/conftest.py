0a787ade88d3b0b915117ffe5e4eb18353fad983
Eimantas Zilis <zilis.eimantas@gmail.com>
2019-10-17 08:47:28 +0100

    Added mock_path fixture to conftest.py

--- unit_tests/conftest.py
+++ unit_tests/conftest.py
@@ -2,10 +2,15 @@ import pytest
 
 from unittest.mock import patch, Mock
 
-from unit_tests.mocks import MockUser
+from unit_tests.mocks import MockPath, MockUser
 
 
 @pytest.fixture
 def mock_user(monkeypatch, tmpdir):
     monkeypatch.chdir(tmpdir)
     yield MockUser(tmpdir)
+
+@pytest.fixture
+def mock_path(monkeypatch, tmpdir):
+    monkeypatch.chdir(tmpdir)
+    yield MockPath(tmpdir)
