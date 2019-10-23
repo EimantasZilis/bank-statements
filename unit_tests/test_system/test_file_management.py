5b37f97b2ad42a50323e77fc36a58288e6ee1005
Eimantas Zilis <zilis.eimantas@gmail.com>
2019-10-23 08:57:01 +0100

    Refactored test_file_management.py to use mocks module

--- unit_tests/test_system/test_file_management.py
+++ unit_tests/test_system/test_file_management.py
@@ -1,126 +1,83 @@
 import os
 import pathlib
 import pytest
-import shutil
-import tempfile
 import datetime
 
 from system.file_management import Path, File, Jdict
+from unit_tests.sample import SampleFile, SamplePath
 
-
-USERPATH = str(pathlib.Path.home())
-
-@pytest.fixture
-def temp_new_dir():
-    """ Setup a new temporary directory during setup.
-    Remove temporary directory during teardown. """
-    dirpath = tempfile.mkdtemp()
-    yield dirpath
-    shutil.rmtree(dirpath)
-
-@pytest.fixture(scope='function')
-def upath(temp_new_dir):
-    """ Setup a new temporary directory and set it as the
-    user path. Re-instate the previous user path
-    during teardown. """
-    config = Jdict("u_paths")
-    common_path = config.lookup("COMMON")
-    config.update("COMMON", temp_new_dir)
-    config.write()
-    yield temp_new_dir
-    config.update("COMMON", common_path)
-    config.write()
 
 class TestPath:
-    creatable = [False, False, False, False, False, True, True]
-    paths = ["em:\\x", None, 1, 0, "", USERPATH]
 
-    @pytest.fixture
-    def object(self, path):
-        """ Create Path object with a given path"""
-        return Path(path)
+    @staticmethod
+    @pytest.mark.parametrize("path", SamplePath.get_all())
+    def test_var_path(mock_path, path):
+        assert Path(path).path == path
 
-    @pytest.mark.parametrize("path", paths)
-    def test_var_path(self, upath, object, path):
-        assert object.path == path
+    @staticmethod
+    @pytest.mark.parametrize("path,creatable", SamplePath.creatable())
+    def test_fx_exists_or_is_creatable(mock_path, path, creatable):
+        assert Path(path).exists_or_is_creatable() == creatable
 
-    @pytest.mark.parametrize("path,tcreatable", list(zip(paths, creatable)))
-    def test_fx_exists_or_is_creatable(self, upath, object, tcreatable):
-        assert object.exists_or_is_creatable() == tcreatable
+    @staticmethod
+    def test_fx_init_dirs(tmpdir):
+        def path_fx_init_dirs(dir, prev_expected, now_expected):
+            prev_exists = os.path.exists(dir)
+            path_object = Path(dir)
+            path_object.init_dirs()
+            now_exists = os.path.exists(dir)
+            assert prev_exists == prev_expected and now_exists == now_expected
 
-    def path_fx_init_dirs(self, dir, prev_expected, now_expected):
-        prev_exists = os.path.exists(dir)
-        path_object = Path(dir)
-        path_object.init_dirs()
-        now_exists = os.path.exists(dir)
-        assert prev_exists == prev_expected and now_exists == now_expected
+        # Test existing paths
+        path_fx_init_dirs(tmpdir, True, True)
+        # Test new paths
 
-    def test_fx_init_dirs_1_existing(self, temp_new_dir):
-        self.path_fx_init_dirs(temp_new_dir, True, True)
-
-    def test_fx_init_dirs_2_new(self, temp_new_dir):
-        newpath = os.path.join(temp_new_dir, "subfolder")
-        self.path_fx_init_dirs(newpath, False, True)
+        newpath = os.path.join(tmpdir, "subfolder")
+        path_fx_init_dirs(newpath, False, True)
 
 
 class TestFile:
-    type_mappings = [("", ""), ("D", "Data"), ("P", "Plot"), ("X", "")]
-    types = ["", "D", "P", "X"]
-    fnames = ["sysfile.txt", "datafile.txt", "plotfile.txt", "xfile.txt"]
-    sfiles = [True, False, False, True]
 
-    @pytest.fixture
-    def bpath(self, upath, sysfile):
-        system_path = os.path.join(os.getcwd(), "system", "configuration")
-        mapping = {True: system_path, False: upath}
-        return mapping[sysfile]
+    @staticmethod
+    @pytest.mark.parametrize("system_file", [True, False])
+    def test_var_system_file(mock_file, system_file):
+        assert File(system_file=system_file).system_file == system_file
 
-    def test_var_expected_extension(self):
-        file_object = File()
-        assert file_object.expected_extension == None
+    @staticmethod
+    @pytest.mark.parametrize("system_file", [True, False])
+    def test_fx_base_path(mock_file, system_file):
+        file_object = File(system_file=system_file)
+        assert file_object.base_path() == mock_file.base_path(system_file)
 
-    @pytest.mark.parametrize("sfile", sfiles)
-    def test_var_system_file(self, sfile):
-        file_object = File(system_file=sfile)
-        assert file_object.system_file == sfile
+    @staticmethod
+    @pytest.mark.parametrize("with_file", [True, False])
+    @pytest.mark.parametrize("system_file", [True, False])
+    @pytest.mark.parametrize("filename", SampleFile.FILENAMES)
+    @pytest.mark.parametrize("subfolder", SampleFile.SUBFOLDERS)
+    @pytest.mark.parametrize("type_id,type_name", SampleFile.type_dict())
+    def test_fx_file_pointer(mock_file, with_file, system_file,
+                             filename, type_id, type_name, subfolder):
+        base_path = mock_file.base_path(system_file)
+        filepath = os.path.join(subfolder, filename)
+        file_object = File(filepath, type_id, system_file)
+        expected = os.path.join(base_path, type_name, subfolder)
+        expected = os.path.join(expected, filename) if with_file else expected
+        assert file_object.file_pointer(with_file=with_file) == expected
 
-    @pytest.mark.parametrize("sysfile", sfiles)
-    def test_fx_base_path(self, upath, sysfile, bpath):
-        file_object = File(system_file=sysfile)
-        assert file_object.base_path() == bpath
+    @staticmethod
+    @pytest.mark.parametrize("filename", SampleFile.FILENAMES)
+    @pytest.mark.parametrize("subdir", SampleFile.SUBFOLDERS)
+    @pytest.mark.parametrize("type_code,type_name", SampleFile.type_dict())
+    def test_fx_parse_inputs(mock_file, filename, subdir, type_code, type_name):
+        filepath = os.path.join(subdir, filename)
+        file_object = File(filepath, type_code)
+        file_check_ok = file_object.filename == filename
+        subfolder_check_ok = file_object.subfolders == subdir
+        type_check_ok = file_object.type == type_name
+        assert all([file_check_ok, subfolder_check_ok, type_check_ok])
 
-    @pytest.mark.parametrize("sysfile", [True, False])
-    @pytest.mark.parametrize("subdirs", ["", "sub1", "sub1\\sub2"])
-    @pytest.mark.parametrize("tid,ftype", type_mappings)
-    def test_fx_file_pointer_with_file(self, bpath, sysfile, ftype, tid, subdirs):
-        filename = "somefile.txt"
-        filepath = os.path.join(subdirs, filename)
-        file_object = File(filepath, tid, sysfile)
-        expected = os.path.join(bpath, ftype, subdirs, filename)
-        assert file_object.file_pointer() == expected
-
-    @pytest.mark.parametrize("sysfile", [True, False])
-    @pytest.mark.parametrize("subdirs", ["", "sub1", "sub1\\sub2"])
-    @pytest.mark.parametrize("tid,ftype", type_mappings)
-    def test_fx_file_pointer_without_file(self, bpath, sysfile, ftype, tid, subdirs):
-        filename = "somefile.txt"
-        filepath = os.path.join(subdirs, filename)
-        file_object = File(filepath, tid, sysfile)
-        expected = os.path.join(bpath, ftype, subdirs)
-        assert file_object.file_pointer(False) == expected
-
-    @pytest.mark.parametrize("filename", ["", "somefile.txt"])
-    @pytest.mark.parametrize("subdirs", ["", "sub1", "sub1\\sub2"])
-    @pytest.mark.parametrize("tid,ftype", type_mappings)
-    def test_fx_parse_inputs(self, upath, filename, subdirs, ftype, tid):
-        filepath = os.path.join(subdirs, filename)
-        file_object = File(filepath, tid)
-        file_check = file_object.filename == filename
-        subfolders_check = file_object.subfolders == subdirs
-        type_check = file_object.type == ftype
-        assert file_check and subfolders_check and type_check
-
-    @pytest.mark.parametrize("type_id,filetype", type_mappings)
-    def test_fx_get_type_name(self, upath, filetype, type_id):
-        file_object = File(type=type_id)
-        assert file_object.get_type_name() == filetype
+    @staticmethod
+    @pytest.mark.parametrize("type_code,type_name", SampleFile.type_dict())
+    def test_fx_get_type_name(mock_file, type_name, type_code):
+        file_object = File(type=type_code)
+        assert file_object.get_type_name() == type_name
